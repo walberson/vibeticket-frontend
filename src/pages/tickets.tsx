719a4c6d313d1d171e6b3948264e62c318e3044d
walberson <walberson.mv@gmail.com>
2023-05-03 14:07:22 -0300

    first commit

---
+++ src/pages/tickets.tsx
@@ -0,0 +1,14 @@
+export default function Tickets() {
+  return (
+    <div className="flex flex-col flex-1 min-h-screen ">
+      <div className="px-4 py-4 bg-slate-100">
+        <div className="flex flex-row items-center justify-between">
+        <h1 className="text-black">VibeAsk</h1>
+        <button className="px-4 bg-white py-2 text-[#835AFD] text-sm border-[0.5px] border-[#835AFD] rounded-lg">
+          Abrir Ticket
+        </button>
+        </div>
+      </div>
+    </div>
+  );
+}
