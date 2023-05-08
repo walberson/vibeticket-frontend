ebe21f514a82f13352d7332d88c5283c529b9a64
Lucas Siqueira <lucas.siqueira@vibetecnologia.com>
2023-05-08 09:19:56 -0300

    add button filter

--- src/pages/history.tsx
+++ src/pages/history.tsx
@@ -1,14 +1,25 @@
 import { Ticket } from "@/components/Ticket";
+import { IoFilterOutline } from 'react-icons/io5'
 
 export default function History() {
   return (
     <div className="grid grid-cols-1 gap-4 content-center w-2/4">
       {/* ---------Formulário--------- */}
-      <form className=" w-2/4">
-        <h1 className="text-text-color text-2xl font-bold leading-9">
-          Histórico
-        </h1>
-        <div className="space-y-12">
+      <form className=" w-full">
+        <div className="flex justify-between w-full mt-5">
+          <h1 className="text-text-color text-2xl font-bold leading-9">
+            Histórico
+          </h1>
+          <button
+          type="button"
+          className="flex justify-center items-center w-24 h-10 ml-24 border-solid border-2 border-gray-200 rounded-md"
+          >
+            <IoFilterOutline />
+            <h4 className="ml-2">Filtrar</h4>
+            </button>
+          <div />
+        </div>
+        <div className="mt-50">
           <div className="pb-12">
             <div className="mt-10 grid grid-cols-1">
               <div className="flex w-full mb-5">
@@ -26,7 +37,7 @@ export default function History() {
                       placeholder="#XXXXXX"
                       id="first-name"
                       autoComplete="given-name"
-                      className="block w-60 rounded-md border-0 py-1.5 text-gray-900 font-normal text-base shadow-sm ring-1 ring-inset ring-gray-300 placeholder:pl-2 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                      className="block w-60 rounded-md border-0 py-1.5 text-gray-900 font-normal text-base shadow-sm ring-1 ring-inset ring-gray-300 placeholder:pl-2 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-gray-200 sm:text-sm sm:leading-6"
                     />
                   </div>
                 </div>
@@ -43,7 +54,7 @@ export default function History() {
                       id="country"
                       name="country"
                       autoComplete="country-name"
-                      className="block w-60 h-9 mr-5 rounded-md py-1.5 border-0 text-gray-900 font-normal text-base shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6"
+                      className="block w-60 h-9 mr-5 rounded-md py-1.5 border-0 text-gray-900 font-normal text-base shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-gray-200 sm:max-w-xs sm:text-sm sm:leading-6"
                     >
                       <option>Hernanda</option>
                       <option>Renan</option>
@@ -64,7 +75,7 @@ export default function History() {
                       id="country"
                       name="country"
                       autoComplete="country-name"
-                      className="block w-60 h-9 rounded-md border-0 py-1.5 text-gray-900 font-normal text-base shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6"
+                      className="block w-60 h-9 rounded-md border-0 py-1.5 text-gray-900 font-normal text-base shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-gray-200 sm:max-w-xs sm:text-sm sm:leading-6"
                     >
                       <option>Aberto</option>
                       <option>Fechado</option>
@@ -87,7 +98,7 @@ export default function History() {
                       id="country"
                       name="country"
                       autoComplete="country-name"
-                      className="block w-60 h-9 mr-5 rounded-md border-0 py-1.5 text-gray-900 font-normal text-base shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6"
+                      className="block w-60 h-9 mr-5 rounded-md border-0 py-1.5 text-gray-900 font-normal text-base shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-gray-200 sm:max-w-xs sm:text-sm sm:leading-6"
                     >
                       <option>Férias</option>
                       <option>Atestado</option>
@@ -110,7 +121,7 @@ export default function History() {
                       // placeholder="#XXXXXX"
                       id="first-name"
                       autoComplete="given-name"
-                      className="block w-60 h-9 mr-5 rounded-md border-0 py-1.5 text-gray-900 font-normal text-base shadow-sm ring-1 ring-inset ring-gray-300 placeholder:pl-2 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                      className="block w-60 h-9 mr-5 rounded-md border-0 py-1.5 text-gray-900 font-normal text-base shadow-sm ring-1 ring-inset ring-gray-300 placeholder:pl-2 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-gray-200 sm:text-sm sm:leading-6"
                     />
                   </div>
                 </div>
@@ -129,7 +140,7 @@ export default function History() {
                       // placeholder="#XXXXXX"
                       id="first-name"
                       autoComplete="given-name"
-                      className="block w-60 h-9 rounded-md border-0 py-1.5 text-gray-900 font-normal text-base shadow-sm ring-1 ring-inset ring-gray-300 placeholder:pl-2 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                      className="block w-60 h-9 rounded-md border-0 py-1.5 text-gray-900 font-normal text-base shadow-sm ring-1 ring-inset ring-gray-300 placeholder:pl-2 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-gray-200 sm:text-sm sm:leading-6"
                     />
                   </div>
                 </div>
@@ -147,7 +158,7 @@ export default function History() {
                       id="country"
                       name="country"
                       autoComplete="country-name"
-                      className="block w-60 h-9 mr-5 rounded-md border-0 py-1.5 text-gray-900 font-normal text-base shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6"
+                      className="block w-60 h-9 mr-5 rounded-md border-0 py-1.5 text-gray-900 font-normal text-base shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-gray-200 sm:max-w-xs sm:text-sm sm:leading-6"
                     >
                       <option>Urgente</option>
                       <option>Urgente</option>
@@ -168,7 +179,7 @@ export default function History() {
                       id="country"
                       name="country"
                       autoComplete="country-name"
-                      className="block w-60 h-9 rounded-md border-0 py-1.5 text-gray-900 font-normal text-base leading-6 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6"
+                      className="block w-60 h-9 rounded-md border-0 py-1.5 text-gray-900 font-normal text-base leading-6 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-gray-200 sm:max-w-xs sm:text-sm sm:leading-6"
                     >
                       <option>Hernanda</option>
                       <option>Renan</option>
